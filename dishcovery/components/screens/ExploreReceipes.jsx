d68d9dd29bc164292781fce5cfc92c567c38d802
coderda-717 <jamesstephen7175@gmail.com>
2025-10-25 21:45:23 +0100

    2nd

---
+++ dishcovery/components/screens/ExploreReceipes.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { useRouter } from 'expo-router';
+
+export default function Home() {
+  const router = useRouter();
+  
+
+  
+
+  return (
+  );
+}
+
+const styles = StyleSheet.create(
+  
+);
