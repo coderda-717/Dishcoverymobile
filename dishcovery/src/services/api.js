fbb185d76cddd9ec7ab4f28ebdd113af81f61046
Coding <150249195+Dammy-wise@users.noreply.github.com>
2025-12-22 12:29:15 +0100

    commit5

--- dishcovery/src/services/api.js
+++
@@ -1,4 +0,0 @@
-import axios from "axios";
-const api = axios.create({ baseURL: ProcessingInstruction.env.API_URL || "https://api.example.com"});
-// request/response interceptors to add auth token
-export default api;
